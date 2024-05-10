783e60614330e62762b8b327d248a022706e1a9a
deepak <deepok626@gmail.com>
2024-05-10 21:27:47 +0530

    all done

--- frontend/src/components/Facilities.jsx
+++ frontend/src/components/Facilities.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 
 const Facilities = () => {
+    const facilityList = [
+        "Follow Safety Rules: Always abide by the safety rules and guidelines set by the water park. Pay attention to lifeguards' instructions and signage posted throughout the park.",
+        "Wear Appropriate Attire: Wear proper swimwear and attire suitable for water activities. Avoid loose clothing or accessories that may pose a safety hazard on rides.",
+        "Stay Hydrated: Drink plenty of water throughout the day to stay hydrated, especially when engaging in physical activities under the sun.",
+        "Keep an Eye on Children: Supervise children closely at all times, especially around water attractions. Ensure they wear appropriate flotation devices if required.",
+        "Follow Ride Instructions: Listen carefully to ride attendants' instructions before boarding water slides or attractions. Follow all safety protocols, including seating arrangements and weight limits.",
+        "Use Locker Facilities: Utilize locker facilities to store valuables securely while enjoying water activities. Avoid bringing unnecessary items into the water park.",
+        "Practice Good Hygiene: Shower before entering pools or attractions to maintain cleanliness and prevent the spread of germs. Follow proper restroom etiquette and dispose of trash in designated bins.",
+        "Have Fun Responsibly: Enjoy the water park's attractions and activities responsibly. Respect other guests, follow park rules, and refrain from disruptive behavior that may endanger yourself or others.",
+        "Apply Sunscreen: Protect your skin from sunburn by applying sunscreen with a high SPF rating. Reapply sunscreen regularly, especially after swimming.",
+    ]
     return (
         <>
-            <div className='text-center p-4 space-y-6  lg:flex place-content-center items-center gap-6 '>
+            <div className='text-center p-4 space-y-6   place-content-center items-center gap-6 '>
                 <div className='lg:text-left space-y-4  w-auto'>
 
                     <h3 className='font-medium text-[1.2rem] w-auto text-primary sm:text-3xl'>Facilities</h3>
                     <h2 className='font-bold text-2xl w-auto'>Simple do’s for a
                         smooth ride!</h2>
-                    <ul className='font-normal w-auto text-secondary text-sm  sm:text-base sm:font-normal'>
-                        <li>Lorem Ipsum is simply dummy text of the printing
-                            and typesetting industry.</li>
-                        <li>Lorem Ipsum is simply dummy text of the printing
-                            and typesetting industry.</li>
-                        <li>Lorem Ipsum is simply dummy text of the printing
-                            and typesetting industry.</li>
-                        <li>Lorem Ipsum is simply dummy text of the printing
-                            and typesetting industry.</li>
-                        <li>Lorem Ipsum is simply dummy text of the printing
-                            and typesetting industry.</li>
-                    </ul>
+                    <ol className='font-normal w-auto text-secondary text-sm space-y-2  sm:text-base sm:font-normal'>
+                       
+                            {
+                                facilityList.map((item,index)=>(
+                                    <>
+                                    <li> <span className='text-black'>{index +1} -</span> { item}</li>
+                                    </>
+                                ))
+                            }
+                    </ol>
                 </div>
                 <div className='grid grid-cols-2 gap-6 justify-center'>
                     <div className='flex justify-center flex-col items-center gap-2 rounded shadow-xl px-4 py-4 min-w-[138px]'>
